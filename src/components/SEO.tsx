708625cd09c7a4fdf47bf894b5b1a1e34c82b5ff
Aaron Kigler <aaronkigler@gmail.com>
2023-08-07 22:01:49 -0400

    SEO 7

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -15,18 +15,16 @@ const SEO: React.FC = () => {
           url: "https://ourshare.ai", // Replace with your website URL
           title: "Ourshare",
           description: "Ourshare - Diversify your equity",
-          images: [
-            {
-              url: "/images/RECT_SEO.svg",
-              width: 800,
-              height: 600,
-              alt: "Ourshare",
-            },
-          ],
         }}
       />
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
+        <Image
+          src="/images/RECT_SEO.svg"
+          width={800}
+          height={600}
+          alt="Ourshare"
+        />
       </Head>
     </>
   );
