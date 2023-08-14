7329d31c08bff9b6e48c02555717f4960fb43379
Aaron Kigler <aaronkigler@gmail.com>
2023-08-14 13:04:46 -0400

    added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "../styles/styles.css";
 import SEO from "../components/SEO";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <SEO />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
